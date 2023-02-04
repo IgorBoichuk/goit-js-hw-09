8783a37f72ec4b13d53e131e836fb6a27b6301ec
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-04 20:48:43 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
+const bodyColor = document.querySelector('body');
 
+bodyColor.addEventListener('click', event => {
+  console.log(event.target.textContent);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
