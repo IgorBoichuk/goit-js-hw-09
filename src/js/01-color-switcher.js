9bf3187f4f12fc34054ae9c85b5a148bfbed280d
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-04 21:12:30 +0200

    11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,13 @@
+import { throttle } from 'lodash';
+
 const bodyColor = document.querySelector('body');
 
-bodyColor.addEventListener('click', event => {
-  console.log(event.target.textContent);
-});
+bodyColor.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  console.log(bodyColor);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
