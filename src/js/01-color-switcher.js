d3bb5b0ef24b10c4e8d8488f19f78deef5562382
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-05 18:39:07 +0200

    9.1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,21 @@
-import { throttle } from 'lodash';
+import throttle from 'lodash';
 
 const bodyColor = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
-bodyColor.addEventListener('click', changeBodyColor);
+let timerId = null;
 
-function changeBodyColor() {
-  bodyColor.style.backgroundColor = getRandomHexColor();
-  console.log(bodyColor);
-}
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log('timer stoped');
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
