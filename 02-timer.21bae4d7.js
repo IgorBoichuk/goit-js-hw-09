f43ec88d78de18dc3c4324c0f47d2987ac0b2559
IgorBoichuk <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-06 22:24:06 +0000

    Deploying to gh-pages from @ IgorBoichuk/goit-js-hw-09@9059c4359f4fdbdf5311d84e54949c2682fc65a3 🚀

--- 02-timer.21bae4d7.js
+++
@@ -1,2 +0,0 @@
-setInterval((()=>{console.log("ksdjcnskjdncksjcn")}),1e3);
-//# sourceMappingURL=02-timer.21bae4d7.js.map
--- 02-timer.4213762f.js
+++
@@ -1,2 +0,0 @@
-setInterval((function(){console.log("ksdjcnskjdncksjcn")}),1e3);
-//# sourceMappingURL=02-timer.4213762f.js.map
